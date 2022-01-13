f1df725f3a32acb1cb6d3fd9be8c3964d604f31e
redapy <baha.redaaaa@gmail.com>
2022-01-13 14:12:55 +0100

    add matched proprety

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import Card from './components/Card';
 
 const cardImages = [
-  { "src": "/img/helmet-1.png" },
-  { "src": "/img/potion-1.png" },
-  { "src": "/img/ring-1.png" },
-  { "src": "/img/scroll-1.png" },
-  { "src": "/img/shield-1.png" },
-  { "src": "/img/sword-1.png" },
+  { "src": "/img/helmet-1.png", 'matched': false },
+  { "src": "/img/potion-1.png", 'matched': false },
+  { "src": "/img/ring-1.png", 'matched': false },
+  { "src": "/img/scroll-1.png", 'matched': false },
+  { "src": "/img/shield-1.png", 'matched': false },
+  { "src": "/img/sword-1.png", 'matched': false },
 ]
 
 function App() {
@@ -43,15 +43,24 @@ function App() {
   useEffect(() => {
     if (choiceTwo && choiceOne) {
       if (choiceOne.src === choiceTwo.src) {
-        console.log('those cards match')
+        setCards(prevcards => (
+          prevcards.map(card => {
+            if (card.src === choiceOne.src) {
+              return {...card, matched: true}
+            } else {
+              return card
+            }
+          })
+        ))
         reset()
       } else {
-        console.log('those cards do not match')
         reset()
       }
     }
-
   },[choiceTwo, choiceOne])
+
+  console.log(cards)
+  console.log(turns)
   
   return (
     <div className="App">
